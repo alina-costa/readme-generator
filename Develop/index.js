05fe61d603963da0c66030d449891cdbfaaadd02
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-08-14 17:17:20 -0400

    not sure whats wrong

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+// const writeFile = require("./utils/generateMarkdown.js");
 
-const promptUser = (printData) => {
+const promptUser = (questions) => {
   return inquirer.prompt([
     {
       type: "input",
@@ -57,19 +57,52 @@ const promptUser = (printData) => {
       message: "What licenses cover this project?",
       choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
     },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
   ]);
-  console.log(printData);
 };
 
-// TODO: Create an array of questions for user input
-// const questions = [];
-
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// const writeToFile = (fileName, data) => {
+//   fs.writeFile(fileName, data, (err) => {
+//     if (err) {
+//       return;
+//     } else {
+//       console.log("README file created!");
+//     }
+//   });
+// };
+
+const writeToFile = (fileName, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: "File created!",
+      });
+    });
+  });
+};
 
 // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+  promptUser().then((data) => {
+    writeToFile("README.md", generateMarkdown(data));
+  });
+}
 
-// Function call to initialize app
-// init();
-promptUser();
+// // Function call to initialize app
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,50 @@
+// const fs = require("fs");
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {
+//   if (license === "None") {
+//     return "";
+//   } else {
+//     return `## License
+//     ${license}`;
+//   }
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you would like to contact me with any questions, please reach out.
+  Email: ${data.email}
+  Github: https://github.com/${data.github}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown();
